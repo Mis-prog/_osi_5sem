9303873c0c6249389536861d91de4c7ca2720465
Mis-prog <akmurzinmihail@gmail.com>
2024-01-13 18:23:15 +0500

    переход с make на cmake

---
+++ lab5/src/client.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "src_lib/calculation.h"
+
+using namespace std;
+
+int  main(){
+    cout << "ffsddf" << endl;
+    return 0;
+}
---
+++ lab5/src/server.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "src_lib/calculation.h"
+
+using namespace std;
+
+int  main(){
+    cout << "ffsddf" << endl;
+    cout << task2()->dx_mean;
+    return 0;
+}
