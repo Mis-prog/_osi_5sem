4b73953d70ce4e06a734c33838e00a01d2de95c0
Mis-prog <akmurzinmihail@gmail.com>
2024-01-15 13:15:34 +0500

    готовая 5 лаба

--- lab5/src/client.cpp
+++ lab5/src/client.cpp
@@ -18,8 +18,6 @@ using std::istream;
 void set_name(json &user);
 
 int main() {
-    json test;
-    test={{0.01:{time:12}}};
     io_service service;
     ip::tcp::socket sock(service);
     ip::tcp::endpoint ep(ip::address::from_string("127.0.0.1"), 8001);
@@ -37,31 +35,48 @@ int main() {
     is >> message;
 
     if (message["message"] == "true") {
-        string data_message;
-        cout << "Введите номер задания (1,2)\n";
-        cin >> data_message;
-        message.clear();
-        message["command"] = data_message;
-        write(sock, buffer(message.dump() + '\n'));
-        if (data_message == "1") {
-            streambuf buf;
-            read_until(sock, buf, '\n');
-            istream is(&buf);
-            json message;
-            is >> message;
-            cout << message << endl;
+        while (true) {
+            string data_message;
+            cout << "Введите номер задания (1,2)\n";
+            cin >> data_message;
+            message.clear();
+            message["command"] = data_message;
+            write(sock, buffer(message.dump() + '\n'));
+            if (data_message == "1") {
+                streambuf buf;
+                read_until(sock, buf, '\n');
+                istream is(&buf);
+                json message;
+                is >> message;
+                cout << message << endl;
 
-            double x, y, z;
-            cin >> x >> y >> z;
-            json data;
-            data["x"] = x;
-            data["y"] = y;
-            data["z"] = z;
+                double x, y, z;
+                cin >> x >> y >> z;
+                json data;
+                data["x"] = x;
+                data["y"] = y;
+                data["z"] = z;
 //            cout << data << endl;
-            write(sock, buffer(data.dump() + '\n'));
-        }
-        if (data_message == "2") {
+                write(sock, buffer(data.dump() + '\n'));
 
+                streambuf buf_task1_data;
+                read_until(sock, buf_task1_data, '\n');
+                istream is_task1_data(&buf_task1_data);
+                string message_data;
+                is_task1_data >> message_data;
+                nlohmann::json parsedJson = nlohmann::json::parse(message_data);
+                std::cout << parsedJson.dump(4) << std::endl;
+            }else if (data_message == "2") {
+                streambuf buf_task2_data;
+                read_until(sock, buf_task2_data, '\n');
+                istream is_task1_data(&buf_task2_data);
+                string message_data;
+                is_task1_data >> message_data;
+                nlohmann::json parsedJson = nlohmann::json::parse(message_data);
+                std::cout << parsedJson.dump(7) << std::endl;
+            }else{
+                break;
+            }
         }
 
     } else {
